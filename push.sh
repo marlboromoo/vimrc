9fd497b8f5be9105773d6be18e67c94fd89481df
timothy.lee <marlboromoo@gmail.com>
2012-05-03 17:09:35 +0800

    ask before push

--- push.sh
+++ push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [[ ! -z "$1" ]]; then
-    ./make_doc.sh
+    read -p 'Create document? (y/n)' ANSWER
+    if [[ "$ANSWER" == 'y' ]]; then
+        ./make_doc.sh
+    fi
     git add .
     git commit -m "$1"
     read -p 'Push to GitHub? (y/n)' ANSWER

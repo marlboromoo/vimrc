21aef2a356952b1215e367bae083062662342cbd
timothy.lee <marlboromoo@gmail.com>
2013-05-28 14:58:12 +0800

    Refactoring code.

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
+VUNDLE_PATH=~/.vim/bundle/vundle
+VUNDLE_REPO=http://github.com/gmarik/vundle.git
 FILES='.vim .vimrc .gvimrc'
+CWD=$(dirname $0)
+
 for file in $FILES
 do
     mv ~/$file{,.bak} 2>/dev/null
-    ln -s ./vimrc/$file ./
+    ln -s $CWD/$file ~/
 done
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+rm -rf $VUNDLE_PATH
+git clone $VUNDLE_REPO $VUNDLE_PATH
 
 

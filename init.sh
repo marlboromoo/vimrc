ce15d60330f5008a3f276f750c9614951db06a5e
timothy.lee <marlboromoo@gmail.com>
2013-05-28 16:59:31 +0800

    Fix error soft link.

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 VUNDLE_PATH=~/.vim/bundle/vundle
 VUNDLE_REPO=http://github.com/gmarik/vundle.git
 FILES='.vim .vimrc .gvimrc'
-CWD=$(dirname $0)
+CWD=$(cd $(dirname $0) && pwd)
 
 for file in $FILES
 do
-    mv ~/$file{,.bak} 2>/dev/null
+    mv ~/$file ~/$file.$(date +"%s") 2>/dev/null
     ln -s $CWD/$file ~/
 done
 rm -rf $VUNDLE_PATH
